c0aef6050ab41de47529ab0016bf1cef197f4093
kanemoda <efedenizbaglar45@gmail.com>
2026-01-20 02:22:53 +0300

    particle wall collision and simulation

--- collisions.c
+++ collisions.c
@@ -1,14 +1,66 @@
 #include "raylib.h"
 
+#define WIDTH 900
+#define HEIGHT 600
+#define FPS 60
+
+#define NUM_PARTICLES 15
+
+typedef struct
+{
+    float x,y,r;
+    float vx,vy;
+} Particle;
+
+void DrawParticle(const Particle *p)
+{
+    DrawCircle(p->x,p->y,p->r,WHITE);
+}
+
+void UpdateParticle(Particle *p, float dt)
+{
+    p->x += p->vx * dt;
+    p->y += p->vy * dt;
+
+    //Horizontal Collision With Wall
+    if (p->x <= p->r) 
+    {
+        p->x = p->r;
+        p->vx *= -1;
+    }
+    else if (p->x >= WIDTH - p->r) 
+    {
+        p->x = WIDTH - p->r;
+        p->vx *= -1;
+    }
+
+    //Vertical Collision With Wall
+    if (p->y <= p->r) 
+    {
+        p->y = p->r;
+        p->vy *= -1;
+    }
+    else if (p->y >= HEIGHT - p->r) 
+    {
+        p->y = HEIGHT - p->r;
+        p->vy *= -1;
+    }
+    
+}
+
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
-
+    InitWindow(WIDTH, HEIGHT, "Elastic Collisions");
+    SetTargetFPS(FPS);
+    Particle particle = {300,300,20,200,200};
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawFPS(5,5);
+            ClearBackground(BLACK);
+            UpdateParticle(&particle, dt);
+            DrawParticle(&particle);
         EndDrawing();
     }
 
